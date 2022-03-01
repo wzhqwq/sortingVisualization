ebbf7ad8a7a339be29606336d17552b67bae6b37
wzhqwq <wzhii345@outlook.com>
2022-03-01 19:46:06 +0800

    添加数据极差提示

--- src/algorithm/boxSort.ts
+++ src/algorithm/boxSort.ts
@@ -5,6 +5,10 @@ export default function *boxSort(input: number[], {declare, assign, wait}: Utils
   let home = new BucketType(input)
   let max = input.reduce((a, b) => Math.max(a, b), input[0])
   let min = input.reduce((a, b) => Math.min(a, b), input[0])
+  if (max - min + 1 > 30) {
+    alert("数据极差过大，这会降低渲染速度，且桶太多会影响算法演示效果，请修改数据生成工具的数据范围。")
+    return
+  }
   let buckets = new Array(max - min + 1).fill(0).map(() => new BucketType([]))
   let n = input.length
   declare("结果", home)
